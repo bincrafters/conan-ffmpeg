9670b45ad590b3548d1d2f040f17c7ddc4619591
SSE4 <tomskside@gmail.com>
2017-12-05 00:37:53 +0700

    - Visual Studio build

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,8 @@
 
 from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
+import glob
+import shutil
 
 
 class FFMpegConan(ConanFile):
@@ -22,9 +24,28 @@ class FFMpegConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, "sources")
 
+    def build_requirements(self):
+        if self.settings.os == 'Windows':
+            self.build_requires("msys2_installer/20161025@bincrafters/stable")
+
+    def run(self, command, output=True, cwd=None):
+        if self.settings.compiler == 'Visual Studio':
+            with tools.environment_append({'PATH': [self.deps_env_info['msys2_installer'].MSYS_BIN]}):
+                bash = "%MSYS_BIN%\\bash"
+                vcvars_command = tools.vcvars_command(self.settings)
+                command = "{vcvars_command} && {bash} -c ^'{command}'".format(
+                    vcvars_command=vcvars_command,
+                    bash=bash,
+                    command=command)
+
+                super(FFMpegConan, self).run(command, output, cwd)
+        else:
+            super(FFMpegConan, self).run(command, output, cwd)
+
     def build(self):
         with tools.chdir('sources'):
-            args = ['--prefix=%s' % self.package_folder,
+            prefix = tools.unix_path(self.package_folder) if self.settings.os == 'Windows' else self.package_folder
+            args = ['--prefix=%s' % prefix,
                     '--disable-doc',
                     '--disable-programs',
                     '--enable-pic']
@@ -35,7 +56,7 @@ class FFMpegConan(ConanFile):
             if self.settings.build_type == 'Debug':
                 args.extend(['--disable-optimizations', '--disable-mmx', '--disable-stripping', '--enable-debug'])
             if self.settings.compiler == 'Visual Studio':
-                args.append(' --toolchain=msvc')
+                args.append('--toolchain=msvc')
             if self.settings.arch == 'x86':
                 args.append('--arch=x86')
 
@@ -54,10 +75,23 @@ class FFMpegConan(ConanFile):
     def package(self):
         with tools.chdir("sources"):
             self.copy(pattern="LICENSE")
-            self.copy(pattern="*", dst="include", src="include")
+        if self.settings.compiler == 'Visual Studio' and not self.options.shared:
+            # ffmpeg produces .a files which are actually .lib files
+            with tools.chdir(os.path.join(self.package_folder, 'lib')):
+                libs = glob.glob('*.a')
+                for lib in libs:
+                    shutil.move(lib, lib[:-2] + '.lib')
 
     def package_info(self):
-        self.cpp_info.libs = ['avdevice', 'avfilter', 'avformat', 'avcodec', 'swresample', 'swscale', 'avutil']
+        libs = ['avdevice', 'avfilter', 'avformat', 'avcodec', 'swresample', 'swscale', 'avutil']
+        if self.settings.compiler == 'Visual Studio':
+            if self.options.shared:
+                self.cpp_info.libs = libs
+                self.cpp_info.libdirs.append('bin')
+            else:
+                self.cpp_info.libs = ['lib' + lib for lib in libs]
+        else:
+            self.cpp_info.libs = libs
         if self.settings.os == "Macos":
             self.cpp_info.exelinkflags.append("-framework AppKit")
             self.cpp_info.exelinkflags.append("-framework AudioToolbox")
@@ -74,3 +108,5 @@ class FFMpegConan(ConanFile):
             self.cpp_info.sharedlinkflags = self.cpp_info.exelinkflags
         elif self.settings.os == "Linux":
             self.cpp_info.libs.extend(['dl', 'pthread'])
+        elif self.settings.os == "Windows":
+            self.cpp_info.libs.extend(['ws2_32', 'secur32', 'shlwapi', 'strmiids', 'vfw32'])
