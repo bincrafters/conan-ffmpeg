e0a00d85628fa8cfde644fd10f08edfbdcfde8df
SSE4 <tomskside@gmail.com>
2017-12-04 19:47:05 +0700

    - minimal working prototype (OSX)

--- conanfile.py
+++ conanfile.py
@@ -1,48 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
-    license = "https://github.com/someauthor/somelib/blob/master/LICENSES"
-    exports_sources = ["CMakeLists.txt", "LICENSE"]
+class FFMpegConan(ConanFile):
+    name = "ffmpeg"
+    version = "3.4"
+    url = "https://github.com/bincrafters/conan-ffmpeg"
+    description = "A complete, cross-platform solution to record, convert and stream audio and video"
+    license = "https://github.com/FFmpeg/FFmpeg/blob/master/LICENSE.md"
+    exports_sources = ["LICENSE"]
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    #use static org/channel for libs in conan-center
-    #use dynamic org/channel for libs in bincrafters
-    requires = "OpenSSL/1.0.2l@conan/stable", \
-        "zlib/1.2.11@conan/stable", \
-        "websocketpp/0.7.0@%s/%s" % (self.user, self.channel)
 
     def source(self):
-        source_url = "https://github.com/Microsoft/cpprestsdk"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        source_url = "http://ffmpeg.org/releases/ffmpeg-%s.tar.bz2" % self.version
+        tools.get(source_url)
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def build(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        cmake.configure(source_dir="sources")
-        cmake.build()
+        with tools.chdir('sources'):
+            args = ['--prefix=%s' % self.package_folder,
+                    '--disable-doc',
+                    '--disable-programs',
+                    '--enable-pic']
+            if self.options.shared:
+                args.extend(['--disable-static', '--enable-shared'])
+            else:
+                args.extend(['--disable-shared', '--enable-static', '--pkg-config-flags=--static'])
+            if self.settings.build_type == 'Debug':
+                args.extend(['--disable-optimizations', '--disable-mmx', '--disable-stripping', '--enable-debug'])
+            if self.settings.compiler == 'Visual Studio':
+                args.append(' --toolchain=msvc')
+
+            # TODO : options
+            args.append('--disable-sdl2')
+            args.append('--disable-zlib')
+            args.append('--disable-bzlib')
+            args.append('--disable-lzma')
+            args.append('--disable-iconv')
+
+            env_build = AutoToolsBuildEnvironment(self)
+            env_build.configure(args=args)
+            env_build.make()
+            env_build.make(args=['install'])
 
     def package(self):
         with tools.chdir("sources"):
             self.copy(pattern="LICENSE")
             self.copy(pattern="*", dst="include", src="include")
-            self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
-            self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.so*", dst="lib", src="lib", keep_path=False)
-            self.copy(pattern="*.dylib", dst="lib", src="lib", keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Macos":
+            self.cpp_info.exelinkflags.append("-framework AppKit")
+            self.cpp_info.exelinkflags.append("-framework AudioToolbox")
+            self.cpp_info.exelinkflags.append("-framework VideoToolbox")
+            self.cpp_info.exelinkflags.append("-framework CoreVideo")
+            self.cpp_info.exelinkflags.append("-framework CoreMedia")
+            self.cpp_info.exelinkflags.append("-framework CoreImage")
+            self.cpp_info.exelinkflags.append("-framework CoreGraphics")
+            self.cpp_info.exelinkflags.append("-framework CoreFoundation")
+            self.cpp_info.exelinkflags.append("-framework OpenGL")
+            self.cpp_info.exelinkflags.append("-framework Foundation")
+            self.cpp_info.exelinkflags.append("-framework AVFoundation")
+            self.cpp_info.exelinkflags.append("-framework Security")
+            self.cpp_info.sharedlinkflags = self.cpp_info.exelinkflags
--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,6 +1,19 @@
-// #include <cpprest/json.h>
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavfilter/avfilter.h>
+#include <libavdevice/avdevice.h>
+#include <libswresample/swresample.h>
+#include <libswscale/swscale.h>
+}
 
 int main()
 {
-    // const auto parsed_value = web::json::value::parse(U("-22"));
+    avcodec_register_all();
+    av_register_all();
+    avfilter_register_all();
+    avdevice_register_all();
+    swresample_version();
+    swscale_version();
 }
