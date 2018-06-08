75f11c736c321b16a625982256d7b0246e1031bc
SSE4 <tomskside@gmail.com>
2018-06-08 15:12:34 +0700

    - build with non-default options
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -4,9 +4,22 @@
 
 
 from bincrafters import build_template_default
+import platform
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-
+    filtered_builds = []
+    for settings, options, env_vars, build_requires, reference in builder.items:
+        modified_options = options.copy()
+        if platform.system() == 'Linux':
+            modified_options['ffmpeg:iconv'] = 'False'
+            modified_options['ffmpeg:vdpau'] = 'False'
+            modified_options['ffmpeg:vaapi'] = 'False'
+            modified_options['ffmpeg:xcb'] = 'False'
+        elif platform.system() == 'Windows':
+            modified_options['ffmpeg:iconv'] = 'False'
+            modified_options['ffmpeg:qsv'] = 'False'
+        filtered_builds.append([settings, modified_options, env_vars, build_requires])
+    builder.builds = filtered_builds
     builder.run()
