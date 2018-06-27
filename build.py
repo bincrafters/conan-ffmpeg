7e42d12916341e92e3f38e02a2d8d27074c9d97f
Uilian Ries <uilianries@gmail.com>
2018-06-27 01:12:56 -0300

    Add custom build for gcc 8
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- build.py
+++ build.py
@@ -2,11 +2,17 @@
 # -*- coding: utf-8 -*-
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-
+from conans import tools
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    if tools.os_info.is_linux:
+        custom_options = {"ffmpeg:vdpau": False, "ffmpeg:vaapi": False, "ffmpeg:xcb": False}
+        builder.add({'arch': 'x86', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+        builder.add({'arch': 'x86', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+        builder.add({'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+        builder.add({'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
 
     builder.run()
