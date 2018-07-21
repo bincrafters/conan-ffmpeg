b568789be8987588f41df3c70bd8e20e82e963b2
SSE4 <tomskside@gmail.com>
2018-07-21 14:24:35 +0700

    - opus is not supported on Visual Studio 2013
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -6,17 +6,19 @@ import os
 from conans import tools
 from bincrafters import build_template_default
 
+
+def disable_opus(build):
+    build.options.update({'ffmpeg:opus': False})
+    return build
+
+
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
-    if tools.os_info.is_linux and os.getenv("CONAN_GCC_VERSIONS") == 8:
-        if not os.getenv("CONAN_BUILD_POLICY"):
-            os.environ["CONAN_BUILD_POLICY"] = "missing"
-        for shared_option in [False, True]:
-            custom_options = {"ffmpeg:vdpau": False, "ffmpeg:vaapi": False, "ffmpeg:xcb": False, 'ffmpeg:shared': shared_option}
-            builder.add({'arch': 'x86', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
-            builder.add({'arch': 'x86', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
-            builder.add({'arch': 'x86_64', 'build_type': 'Release', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
-            builder.add({'arch': 'x86_64', 'build_type': 'Debug', 'compiler': 'gcc', 'compiler.version': 8}, custom_options)
+
+    if 'CONAN_VISUAL_VERSIONS' in os.environ:
+        if os.environ['CONAN_VISUAL_VERSIONS'] == '12':
+
+            builder.builds = map(disable_opus, builder.items)
 
     builder.run()
